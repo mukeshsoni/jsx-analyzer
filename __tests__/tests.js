6c2347da751b28c6d26dd1686b4c989084e76986
Mukesh Soni <mukeshsoni@gmail.com>
2017-11-25 00:56:57 +0530

    added failing test for function prop

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -5,11 +5,10 @@ describe('asd', () => {
     it('should get jsx string literal prop value', () => {
         let jsxCode = '<div name="someone" ></div>'
         let expectedProps = {
-            name: 'someone',
-            age: 1
+            name: 'someone'
         }
 
-        expect(getPropsFromJsxCode(jsxCode)).toEqual({ name: 'someone' })
+        expect(getPropsFromJsxCode(jsxCode)).toEqual(expectedProps)
     })
 
     it('should get jsx numeric literal prop value', () => {
@@ -38,6 +37,26 @@ describe('asd', () => {
         expect(getPropsFromJsxCode(jsxCode)).toEqual(expectedProps)
     })
 
+    it('should get jsx nested object prop value', () => {
+        let jsxCode =
+            '<div name="someone" age={1} address={{street: "kodihalli", city: "bangalore", pincode: 570000, country: {name: "india", code: "+91"}}}></div>'
+        let expectedProps = {
+            name: 'someone',
+            age: 1,
+            address: {
+                street: 'kodihalli',
+                city: 'bangalore',
+                pincode: 570000,
+                country: {
+                    name: 'india',
+                    code: '+91'
+                }
+            }
+        }
+
+        expect(getPropsFromJsxCode(jsxCode)).toEqual(expectedProps)
+    })
+
     it('should get jsx array prop value', () => {
         let jsxCode =
             '<div name="someone" age={1} address={{street: "kodihalli", city: "bangalore", pincode: 570000 }} repos={[{url: "github/1", name: "repo1"},{url: "github/2", name: "repo2"},]}></div>'
@@ -59,6 +78,16 @@ describe('asd', () => {
                     name: 'repo2'
                 }
             ]
+        }
+
+        expect(getPropsFromJsxCode(jsxCode)).toEqual(expectedProps)
+    })
+
+    it('should get jsx function prop value', () => {
+        let jsxCode =
+            '<div name="someone" onClick={function() {console.log("clicked")}}></div>'
+        let expectedProps = {
+            name: 'someone'
         }
 
         expect(getPropsFromJsxCode(jsxCode)).toEqual(expectedProps)
